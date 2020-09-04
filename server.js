a65c170048d1a3783098ab64129ad61c71ce5bbf
ttreit <14933277+ttreit@users.noreply.github.com>
2020-09-04 13:14:38 -0700

    Add cors middleware

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+
 
 const app = express();
 const port = process.env.PORT || 7199;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
