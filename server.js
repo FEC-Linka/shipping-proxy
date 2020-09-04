e4576fbc5d906c1dfcd5113d4c304661830ac09f
ttreit <14933277+ttreit@users.noreply.github.com>
2020-09-03 18:31:26 -0700

    Add route to handle /:productId

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 })
 
+app.get('/:productId', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+})
+
 app.listen(port, () => {
   console.log(`Etsy Proxy listening on port ${port}`);
 })
